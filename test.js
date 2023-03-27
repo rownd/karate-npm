16742c4e446532eebaed045dd28b45ff10ddd804
Matt Hamann <mhamann@rownd.io>
2023-03-27 11:41:01 -0400

    feat(errors): use jbang fork until new versions of jbang-npm and karate-npm are published

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ var karate = require('./karate');
 karate.version = '1.3.0';
 karate.config.dir = 'karate'
 karate.exec();
+
+try {
+    karate.exec("non-existent-file.feature")
+    console.error('expected error not thrown');
+    process.exit(1);
+} catch (err) {
+    console.log('caught expected error');
+}
